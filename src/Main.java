2b24c112c26b0b6a3d10d6cfb02bb3439a2a6ad2
NILUFER\niluf <niluferbasgul@hotmail.com>
2023-11-26 22:29:24 +0100

    Added second prototype service

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
         prototype.addService(new DataService("data", 300.0, 0.0, 5, 10.24, 5.5, 4, 1024, 5.2,5,5));
         PackageFactory.addPrototype(prototype);
 
+
+        ServicePackage prototype2 = ServicePackage.createPackage("post-paid");
+        prototype2.addService(new VoiceService("voice1", 2.9, 0.0, 2, 20.0, 2.0, 1, 0.0));
+        prototype2.addService(new SmsService("sms1", 200.0, 0.0, 2, 12.24, 2.5, 2,3.5));
+        prototype2.addService(new DataService("data1", 200.0, 0.0, 2, 12.24, 2.5, 4, 1034, 3.2,5,5));
+        PackageFactory.addPrototype(prototype2);
         // Clone a package and display details
         ServicePackage clonedPackage = PackageFactory.findAndClone("post-paid");
         if (clonedPackage != null) {
